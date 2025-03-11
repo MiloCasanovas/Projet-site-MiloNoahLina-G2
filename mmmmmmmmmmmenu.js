001644e387aa38e5a4dbe45f662a5d46a937c6c8
Nolithoast <noahcnoya@gmail.com>
2025-03-11 21:06:53 +0100

    Create mmmmmmmmmmmenu.js

---
+++ mmmmmmmmmmmenu.js
@@ -0,0 +1,7 @@
+function reesto() {
+  // Cacher l'article1
+  document.getElementById("article1").style.display = "none";
+
+  // Afficher le contenu
+  document.getElementById("content").style.display = "block";
+}
